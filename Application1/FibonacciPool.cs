04ac3b694befd37fe056d9b64c5b371fa5d21752
Your Name <igr.khitrov@gmail.com>
2020-10-14 11:28:39 +0300

    Added Application1

---
+++ Application1/FibonacciPool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application1
+{
+    class FibonacciPool
+    {
+
+        public FibonacciPool(int SequenceAmount)
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+    }
+}
---
+++ Application1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequenceAmount = 3;
+            FibonacciPool fp = new FibonacciPool(sequenceAmount);
+            fp.Start();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
