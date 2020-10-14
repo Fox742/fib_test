65013009937499a7d21830f062bfb077c6c000c8
Your Name <igr.khitrov@gmail.com>
2020-10-14 11:19:19 +0300

    Added Fibonacci Controller

---
+++ Application2/Controllers/FibonacciController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FibonacciController : ControllerBase
+    {
+
+        public string Get()
+        {
+            return "Сервис запущен! Воспользуйтесь методом fibonacci/next?n=<int> для получения следующего числа последовательности Фибоначчи";
+        }
+
+        [HttpGet("next")]
+        public string[] Next(int number)
+        {
+            return new string[]
+            {
+                "Next",
+                "element:",
+                number.ToString()
+            };
+        }
+    }
+}
